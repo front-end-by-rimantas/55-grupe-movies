5431d25f21c7a0e21e0bc80becb3dcaf2ddc02d7
Rimantas <belauzas@gmail.com>
2025-07-02 10:25:17 +0300

    Register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,19 +1,13 @@
 import { PageTemplate } from "../templates/PageTemplate.js";
+import { registerForm } from "../ui/forms/registerForm.js";
 import { pageTitle } from "../ui/pageTitle.js";
-import { placeholder } from "../ui/placeholder.js";
 
 export class PageRegister extends PageTemplate {
     main() {
         return `
             <main>
                 ${pageTitle('Register')}
-                ${placeholder('Form', [
-            'username',
-            'email',
-            'password',
-            'sutinku su taisyklemis',
-            'mygtukas',
-        ])}
+                ${registerForm()}
             </main>`;
     }
 }
