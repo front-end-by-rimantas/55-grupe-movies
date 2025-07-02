80fa2dd4b059c50db34d412c52549c802fb1de95
Rimantas <belauzas@gmail.com>
2025-07-02 10:48:56 +0300

    Client side register form logic

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,11 @@ import { registerForm } from "../ui/forms/registerForm.js";
 import { pageTitle } from "../ui/pageTitle.js";
 
 export class PageRegister extends PageTemplate {
+    constructor(req) {
+        super(req);
+        this.pageJS = 'register';
+    }
+
     main() {
         return `
             <main>
