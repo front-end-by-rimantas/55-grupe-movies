069ea7c272da8fbe94728eea0dfd18b6d0544abc
Rimantas <belauzas@gmail.com>
2025-07-08 10:21:42 +0300

    Up

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,7 +1,13 @@
+import { COOKIE_MAX_AGE } from "../../env.js";
 import { PageTemplate } from "../../templates/PageTemplate.js";
 
 export class PageDashboard extends PageTemplate {
     main() {
+        const cookie = this.req.user.login_token_created_at.getTime();
+        const secondsLeft = Math.floor(COOKIE_MAX_AGE - (Date.now() - cookie) / 1000);
+        const seconds = secondsLeft % 60;
+        const minutes = (secondsLeft - seconds) / 60;
+
         return `
             <main>
                <div class="container">
@@ -10,6 +16,7 @@ export class PageDashboard extends PageTemplate {
                             <h1 class="display-1">Welcome to your dashboard!</h1>
                             <p class="display-6">Username: ${this.req.user.username}</p>
                             <p class="display-6">Email: ${this.req.user.email}</p>
+                            <p class="display-6">Likęs sesijos laikas: ${minutes}:${seconds}</p>
                         </div>
                     </div>
                 </div>
