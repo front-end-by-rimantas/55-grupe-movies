b86831bf67bdef64ead95182e7607c3c3c7fe08f
Rimantas <belauzas@gmail.com>
2025-07-14 10:34:07 +0300

    Categories lists

--- src/db/getPublishedCategories.js
+++ src/db/getPublishedCategories.js
@@ -1,13 +1,14 @@
 import { connection } from "../db.js";
 
-export async function getAllCategories() {
+export async function getPublishedCategories() {
     try {
         const sql = `
             SELECT categories.*, 0 AS moviesCount, category_status.name
             FROM categories
             INNER JOIN category_status
-                ON categories.status_id = category_status.id;`;
-        const [result] = await connection.execute(sql);
+                ON categories.status_id = category_status.id
+            WHERE category_status.name = ?;`;
+        const [result] = await connection.execute(sql, ['published']);
         return result;
     } catch (err) {
         return [];
